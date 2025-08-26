b168a5ff476770b29e2de8368af183f3404163be
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-08-26 17:57:21 +0200

    working on cli tool

--- exec.py
+++
@@ -1,4 +0,0 @@
-from src.BEANS.executor import run_executor
-from sys import argv
-
-run_executor(argv[1])
---
+++ src/BEANS/run.py
@@ -0,0 +1,8 @@
+def main():
+    from src.BEANS.executor import run_executor
+    import argparse
+    print("wowowowo")
+
+
+if __name__ == "__main__":
+    main()
