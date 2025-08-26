5e52745be8dea8bbac055d0f3cd3e1ec813e219d
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-08-26 17:07:12 +0200

    added JMP

---
+++ src/BEANS/interpreter/operations/JMP.py
@@ -0,0 +1,12 @@
+from src.BEANS.interpreter.memory.memory import memory
+from src.BEANS.interpreter.memory.registers import registers
+
+from src.BEANS.interpreter.op_api import handle_args
+
+
+class operation:
+
+    @staticmethod
+    def execute_operation(mem: tuple[registers, memory], args, pc_index): # type: ignore
+        args = handle_args(args, ["num"])
+        return args[0]
