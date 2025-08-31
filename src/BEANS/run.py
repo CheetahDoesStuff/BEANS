c23f458394afce560a682b871be6c318b6895145
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-08-31 17:19:13 +0200

    file argument

--- src/BEANS/run.py
+++ src/BEANS/run.py
@@ -4,6 +4,8 @@ def main():
     
     parser = argparse.ArgumentParser(description="Execute a .bean assembly file using the BEANExecutor")
 
+    parser.add_argument("file", type=str, help="Path to the .bean file to execute.")
+
 
 if __name__ == "__main__":
     main()
