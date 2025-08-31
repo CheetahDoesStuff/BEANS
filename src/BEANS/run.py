2382b69086b0520b496a5feb09769ebea3be714d
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-08-31 17:39:17 +0200

    bumped version and args not working

--- src/BEANS/run.py
+++ src/BEANS/run.py
@@ -9,10 +9,10 @@ def main():
     parser = argparse.ArgumentParser(description="Execute a .bean assembly file using the BEANExecutor")
 
     parser.add_argument("file", type=str, help="Path to the .bean file to execute.")
-    parser.add_argument("-m", "--module-path", type=Path, help="The path to the directory containing modules used.", default=Path(user_data_dir("BEANS", "Cheetah")) / "BIOMods")
+    parser.add_argument("-p", "--module-path", type=Path, help="The path to the directory containing modules used.", default=Path(user_data_dir("BEANS", "Cheetah")) / "BIOMods")
     parser.add_argument("-b", "--value-size", type=int, help="The size of each value in bits.", default=16)
-    parser.add_argument("--register-count", type=int, help="The amount of registers the program creates.", default=16)
-    parser.add_argument("--memory-address-count", type=int, help="The amount of memory adresses the program creates.", default=64)
+    parser.add_argument("-r", "--register-count", type=int, help="The amount of registers the program creates.", default=16)
+    parser.add_argument("-m", "--memory-address-count", type=int, help="The amount of memory adresses the program creates.", default=64)
 
     args = parser.parse_args()
 
